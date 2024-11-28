38a42d9b9be2bab89e939032d9a5ad6af84bef4e
DinsLim <dinsdalelim360@gmail.com>
2024-11-28 15:05:03 +0800

    Login Page Uodate

--- Campus Ministry Website/website/framework ver/bootstrap/modules/navigation-panel.php
+++ Campus Ministry Website/website/framework ver/bootstrap/modules/navigation-panel.php
@@ -1,142 +1,135 @@
 <?php
 session_start();
 $username = $_SESSION['username'];
-$upperCase = strtoupper($username);
-$staffID = $_SESSION['Staff_ID'];
-$admin = $_SESSION['is_admin'];
 
-$adminPermission = '';
-if ($admin != 0) {
-    $adminPermission = <<<ADMINPERMISSION
-    <a class="nav-link" href="staff-list.php">
-        <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
-        Staff List
-    </a>
-    <a class="nav-link" href="register.php">
-        <div class="sb-nav-link-icon"><i class="fas fa-user-plus"></i></div>
-        Register Staff
-    </a>
-    ADMINPERMISSION;
-}
+// this is le start of le whole nav panels
+
+///**/
 
 $htmlContent = <<<HTML
-            <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-                <!-- Navbar Brand-->
-                <a class="navbar-brand ps-3" href="main-page.php">Campus Ministry</a>
-                <!-- Sidebar Toggle-->
-                <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-                    
-                <div class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-                    <div style="color:white; ">
-                        <a class="nav-link">
-                            Welcome $upperCase!
+    
+    
+    <nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #1c4966; color: white;">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="main-page.php" style="color: white;">Campus Ministry</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!" style="color: white;"><i class="fas fa-bars"></i></button>
+        <!-- Navbar Search-->
+        <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+                <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
+        <!-- Navbar-->
+        <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;"><i class="fas fa-user fa-fw"></i></a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#!">Settings</a></li>
+                    <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion" style="background-color: #1c4966;" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading" style="color: white;">Core</div>
+                        <a class="nav-link" href="main-page.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt" style="color: white;"></i></div>
+                            Dashboard
+                        </a>
+                        <div class="sb-sidenav-menu-heading" style="color: white;">Interface</div>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns" style="color: white;"></i></div>
+                            Layouts
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down" style="color: white;"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="layout-static.html" style="color: white;">Static Navigation</a>
+                                <a class="nav-link" href="layout-sidenav-light.html" style="color: white;">Light Sidenav</a>
+                            </nav>
+                        </div>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-book-open" style="color: white;"></i></div>
+                            Pages
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down" style="color: white;"></i></div>
+                        </a>
+                        <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth" style="color: white;">
+                                    Authentication
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down" style="color: white;"></i></div>
+                                </a>
+                                <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="index.php" style="color: white;">Login</a>
+                                        <a class="nav-link" href="register.php" style="color: white;">Register</a>
+                                        <a class="nav-link" href="password.php" style="color: white;">Forgot Password</a>
+                                    </nav>
+                                </div>
+                                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError" style="color: white;">
+                                    Error
+                                    <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down" style="color: white;"></i></div>
+                                </a>
+                                <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordionPages">
+                                    <nav class="sb-sidenav-menu-nested nav">
+                                        <a class="nav-link" href="401.html" style="color: white;">401 Page</a>
+                                        <a class="nav-link" href="404.html" style="color: white;">404 Page</a>
+                                        <a class="nav-link" href="500.html" style="color: white;">500 Page</a>
+                                    </nav>
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="sb-sidenav-menu-heading" style="color: white;">Addons</div>
+                        <a class="nav-link" href="charts.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Charts
+                        </a>
+                        <a class="nav-link" href="tables.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-table" style="color: white;"></i></div>
+                            Tables
+                        </a>
+                        <a class="nav-link" href="add-old-event.html" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Add Old Event
+                        </a>
+                        <a class="nav-link" href="old-event-details.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Old Event Details
+                        </a>
+                        <a class="nav-link" href="add-event.html" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Add Event
+                        </a>
+                        <a class="nav-link" href="event-details.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Event Details
+                        </a>
+                        <a class="nav-link" href="staff-list.php" style="color: white;">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area" style="color: white;"></i></div>
+                            Staff List
                         </a>
                     </div>
                 </div>
-                <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <button class="dropdown-item staff-update-btn" data-id="$staffID">Update</button>
-                            <li><hr class="dropdown-divider"/></li>
-                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <div class="sb-sidenav-footer" style="color: white;">
+                    <div class="small">Logged in as:</div>
+                    $username
+                </div>
             </nav>
-            <div id="layoutSidenav">
-                <div id="layoutSidenav_nav">
-                    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                        <div class="sb-sidenav-menu">
-                            <div class="nav">
-                                <a class="nav-link" href="main-page.php">
-                                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                    Dashboard
-                                </a>
-                                
-                                <a class="nav-link" href="add-event.html">
-                                    <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                    Add Event
-                                </a>
+        </div>
 
-                                {$adminPermission}
+HTML;
 
-                            </div>
-                        </div>
-                    </nav>
-                </div>
+//*/  // this is le end of le whole nav panels
 
-
-                <!-- Modal for Updating Staff Information -->
-                <div class="modal fade" id="updateStaffModal" tabindex="-1" aria-labelledby="updateStaffModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="updateStaffModalLabel">Update User Information</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                <form id="updateStaffForm">
-                                    <input type="hidden" id="updateStaffId" name="StaffId">
-
-                                    <!-- First Name -->
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" id="updateStaffFirstName" name="firstName" placeholder="First Name" required>
-                                        <label for="updateStaffFirstName">*First Name</label>
-                                    </div>
-
-                                    <!-- Last Name -->
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" id="updateStaffLastName" name="lastName" placeholder="Last Name" required>
-                                        <label for="updateStaffLastName">*Last Name</label>
-                                    </div>
-
-                                    <!-- Type -->
-                                    <div class="form-floating mb-3">
-                                        <select class="form-control" id="updateStaffType" name="type" required>
-                                            <option value="" disabled selected>Select Type</option>
-                                            <option value="Developer">Developer</option>
-                                            <option value="Admin">Admin</option>
-                                            <option value="Retreat">Retreat</option>
-                                            <option value="Recollection 01">Recollection 01</option>
-                                            <option value="Recollection 02">Recollection 02</option>
-                                        </select>
-                                        <label for="updateStaffType">Type</label>
-                                    </div>
-
-                                    <!-- Email -->
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" id="updateStaffEmail" name="email" placeholder="Email" required >
-                                        <label for="updateStaffEmail">*Email &nbsp; (example@adzu.edu.ph)</label>
-                                    </div>
-
-                                    <!-- Password Groups -->
-                                    <div class="form-floating mb-3 bg-warning">
-                                        <p class="text-center">New Password (when needed)</p>
-                                    </div>
-
-                                    <!-- Password -->
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control" id="updateStaffPassword" name="password" placeholder="Password">
-                                        <label for="updateStaffPassword">New Password</label>
-                                    </div>
-
-                                    <!-- Confirm Password -->
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control" id="updateStaffConfirmPassword" name="confirmPassword" placeholder="Confirm Password">
-                                        <label for="updateStaffConfirmPassword">Confirm Password</label>
-                                    </div>
-
-                                    <!-- Submit Button -->
-                                    <button class="btn btn-primary update-information-btn" >Update Information</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <script src="js/staff-options.js"></script>
-            HTML;
 echo $htmlContent;
+
 ?>
 </html>
